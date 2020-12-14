10f6a72d909bbd637e1f1a18589ef401c1cdf630
natashanugrah <natasha.noverya@binus.ac.id>
2020-12-14 19:20:58 +0700

    yang diubah: seller location

--- app/src/main/java/com/ba11groupj/madproject/helpers/DataHelper.java
+++ app/src/main/java/com/ba11groupj/madproject/helpers/DataHelper.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 // fungsi class ini cuma sebagai hack biar recyclerview di menu history bisa auto refresh
 
-public class DataHelper {
+public class   DataHelper {
 
     public static ArrayList<Transaction> arrTrans = new ArrayList<>();
 
